2df503cdd432767901306180242329a9c00298f7
FelipeBBravim <felipe.bravim@gmail.com>
2021-01-31 06:26:22 -0300

    Project created

---
+++ src/main/java/com/educandoweb/workshop_springbootjpamaven/WorkshopSpringbootJpaMavenApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.workshop_springbootjpamaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringbootJpaMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringbootJpaMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/workshop_springbootjpamaven/WorkshopSpringbootJpaMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.workshop_springbootjpamaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringbootJpaMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
