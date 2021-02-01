d959c80d6f03be6fe9c8a48082ffebe817cc141a
FelipeBBravim <felipe.bravim@gmail.com>
2021-02-01 16:09:32 -0300

    Exception handling - findById

--- src/main/java/com/educandoweb/workshop_springbootjpamaven/services/UserService.java
+++ src/main/java/com/educandoweb/workshop_springbootjpamaven/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.educandoweb.workshop_springbootjpamaven.entities.User;
 import com.educandoweb.workshop_springbootjpamaven.repositories.UserRepository;
+import com.educandoweb.workshop_springbootjpamaven.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,8 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
---
+++ src/main/java/com/educandoweb/workshop_springbootjpamaven/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.educandoweb.workshop_springbootjpamaven.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+}
