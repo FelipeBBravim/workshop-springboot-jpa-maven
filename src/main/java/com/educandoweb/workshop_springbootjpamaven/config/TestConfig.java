774f5e7ea2993e5c9becf2ecef46c7bb9f96f4e0
FelipeBBravim <felipe.bravim@gmail.com>
2021-02-01 13:16:25 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/workshop_springbootjpamaven/config/TestConfig.java
+++ src/main/java/com/educandoweb/workshop_springbootjpamaven/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.educandoweb.workshop_springbootjpamaven.entities.Category;
 import com.educandoweb.workshop_springbootjpamaven.entities.Order;
 import com.educandoweb.workshop_springbootjpamaven.entities.OrderItem;
+import com.educandoweb.workshop_springbootjpamaven.entities.Payment;
 import com.educandoweb.workshop_springbootjpamaven.entities.Product;
 import com.educandoweb.workshop_springbootjpamaven.entities.User;
 import com.educandoweb.workshop_springbootjpamaven.entities.enums.OrderStatus;
@@ -81,5 +82,9 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
